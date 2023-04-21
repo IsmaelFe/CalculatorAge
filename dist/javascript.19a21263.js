f53ed1023b1da25535564fb22975df2d7c7640f7
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-04-21 12:15:38 -0600

    Tratando errores

--- dist/javascript.19a21263.js
+++ dist/javascript.19a21263.js
@@ -2831,7 +2831,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52378" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64929" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
